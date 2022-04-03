09301c0343aea14d72233c4a947e06468eb2b52e
mateusns12 <mateusgms12@gmail.com>
2022-04-03 20:36:15 -0300

    Main updated
    "using namespace std" removed, trying fix bad habits in cpp.

--- main.cpp
+++ main.cpp
@@ -3,15 +3,36 @@
 #include <string.h>
 #include <stdlib.h>
 #include <semaphore.h>
-using namespace std;
 
 pthread_t task_handle;
 pthread_t task2_handle;
 
 sem_t semaforo;
 
-void * task(void * pvParam);
-void * task2(void * pvParam);
+void * task(void * pvParam)
+{
+	std::cout<<"Task 1"<<std::endl;
+	sem_wait(&semaforo);
+	int i = 0;
+	while(i<10){
+		std::cout<<i+1<<std::endl;
+	i++;
+	}
+	sem_post(&semaforo);
+	pthread_exit(NULL);
+}
+
+void * task2(void * pvParam)
+{
+	std::cout<<"Task 2"<<std::endl;
+	sem_wait(&semaforo);
+	int i=0;
+	for (i = 5; i>=0; i--){
+		std::cout<<"Finishing in: "<<i<<std::endl;
+	}
+	sem_post(&semaforo);
+	pthread_exit(NULL);
+}
 
 int main(){
 	sem_init(&semaforo,0,1);
@@ -22,27 +43,3 @@ int main(){
 	sem_destroy(&semaforo);
 	return 0;
 }
-
-void * task(void * pvParam){
-	cout<<"Task 1"<<endl;
-	sem_wait(&semaforo);
-	int i = 0;
-	while(i<10){
-		cout<<i+1<<endl;
-	i++;
-	}
-	sem_post(&semaforo);
-	pthread_exit(NULL);
-}
-
-void * task2(void * pvParam){
-	cout<<"Task 2"<<endl;
-	sem_wait(&semaforo);
-	int i=0;
-	for (i = 5; i>=0; i--){
-		cout<<"finalizando em: "<<i<<endl;
-	}
-	sem_post(&semaforo);
-	pthread_exit(NULL);
-}
-
